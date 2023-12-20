fa57fed16328deb259d8c5655cad6b24ed26773a
ApolloRegis <ApolloRegis0@gmail.com>
2023-12-20 14:15:41 -0500

    ADD BACKGROUND COLOR TO DIVS

--- Etch-A-Sketch.js
+++ Etch-A-Sketch.js
@@ -4,15 +4,14 @@ container.appendChild(box);
 
 for(let i = 0; i < 16; i++) {
     const rsquares = document.createElement('div');
-    rsquares.textContent = 'div';
+    //rsquares.textContent = 'div';
+    rsquares.style.cssText = "background-color: black";
 
     for(let j = 0; j < 15; j++) {
         const csquares = document.createElement('div');
-        csquares.textContent = 'div';
+        //csquares.textContent = 'div';
         rsquares.appendChild(csquares);
     };
     container.appendChild(rsquares);
 };
 
-
-//squares.textContent = 'div';
