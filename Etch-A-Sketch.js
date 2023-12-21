ba63fcd1210ad2651c2f9493b390dcb3f0188113
ApolloRegis <ApolloRegis0@gmail.com>
2023-12-21 18:24:33 -0500

    ADD HOVER EFFECT

--- Etch-A-Sketch.js
+++ Etch-A-Sketch.js
@@ -6,5 +6,9 @@ for(i = 0; i < num; i++) {
     const box = document.createElement('div');
     box.style.cssText = "width: 100px; height: 100px; background-color: blue;";
     box.textContent = 'div';
+
+    box.addEventListener('mouseover', () => {
+        box.style.cssText = "width: 100px; height: 100px; background-color: red;";
+    });
     container.appendChild(box);
 };
